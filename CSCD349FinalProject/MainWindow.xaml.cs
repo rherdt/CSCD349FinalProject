5b27268ec6f0d4475d7efbe8d58f465870ca7462
Rylan <rherdt@itron.com>
2016-04-27 12:31:29 -0700

    Started GUI backgrounds

---
+++ CSCD349FinalProject/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CSCD349FinalProject.States;
+using CSCD349FinalProject.Spaces;
+
+namespace CSCD349FinalProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeButton_Click(object sender, RoutedEventArgs e)
+        {
+            int maxHeight = 10, maxWidth = 10, maxFloors = 10;
+
+            int x, y, z;
+            if (!(int.TryParse(HeightEntryTextBox.Text, out x) && (x > 0 && x <= maxHeight)) || !(int.TryParse(WidthEntryTextBox.Text, out y) && (y > 0 && y <= MaxWidth)) || !(int.TryParse(FloorEntryTextBox.Text, out z) && (z > 0 && z <= maxFloors)))
+            {
+                MessageBox.Show("One or more inputs is invalid.", "Invalid Input");
+                return;
+            }
+            else
+            {
+                int columns = int.Parse(WidthEntryTextBox.Text);
+                int rows = int.Parse(HeightEntryTextBox.Text);
+
+                ISpace currentSpace;
+                Button btn;
+                int i = 0;
+
+                for (int a = 0; a < columns; a++)
+                {
+                    GameBoard.ColumnDefinitions.Add(new ColumnDefinition());
+                }
+
+                for (int b = 0; b < rows; b++)
+                {
+                    GameBoard.RowDefinitions.Add(new RowDefinition());
+                }
+
+                for (int row = 0; row < GameBoard.RowDefinitions.Count; row++)
+                {
+                    for (int column = 0; column < GameBoard.ColumnDefinitions.Count; column++)
+                    {
+                        i++;
+                        currentSpace = new TravelSpace();
+                        currentSpace.getSpace().Width = GameBoard.Width / columns;
+                        currentSpace.getSpace().Height = GameBoard.Width / columns;
+                        currentSpace.getSpace().Fill = new SolidColorBrush(Colors.Aqua);
+                        //Need to make border, or switch back to buttons 
+                        currentSpace.getSpace().VerticalAlignment = VerticalAlignment.Center;
+                        currentSpace.getSpace().SetValue(Grid.ColumnProperty, column);
+                        currentSpace.getSpace().SetValue(Grid.RowProperty, row);
+                        //Need to add text element to each rectangle
+                        //btn = new Button();
+                        // btn.Width = GameBoard.Width / columns;
+                        //btn.Height = GameBoard.Width / columns;
+                        //btn.Content = i.ToString();
+                        
+                        GameBoard.Children.Add(currentSpace.getSpace());
+                    }
+                }
+
+            }//end elserfd
+        }
+    }
+}
---
+++ CSCD349FinalProject/Spaces/TravelSpace.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using CSCD349FinalProject.States;
+
+namespace CSCD349FinalProject.Spaces
+{
+    class TravelSpace : ISpace
+    {
+        private Rectangle space;
+        private IState state;
+
+        public TravelSpace()
+        {
+            this.space = new Rectangle();
+            this.state = new TravelState();
+        }
+
+        public Rectangle getSpace()
+        {
+             return this.space;
+        }
+
+    }
+}
