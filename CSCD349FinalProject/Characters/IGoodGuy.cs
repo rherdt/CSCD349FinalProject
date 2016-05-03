e173477dbbc826c7e3ad113b3c797767d52d9838
Gavin Rouse <gavin.rouse@eagles.ewu.edu>
2016-05-02 22:02:25 -0700

    Added Character functionality and empty inventory item class.

---
+++ CSCD349FinalProject/Characters/IGoodGuy.cs
@@ -0,0 +1,18 @@
+﻿using CSCD349FinalProject.Inventory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCD349FinalProject.Characters
+{
+    interface IGoodGuy : ICharacter
+    {
+        InvItem GetInvItem(int slot);
+        void PickUpInvItem(InvItem item);
+        void DropInvItem(int slot);
+        int NextEmptyInvSlot();
+        bool InventoryFull();
+    }
+}
