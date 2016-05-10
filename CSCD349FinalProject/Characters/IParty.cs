8ff69238bb687c2f76c78f348793677d464f834e
Gavin Rouse <gavin.rouse@eagles.ewu.edu>
2016-05-09 21:46:18 -0700

    added IParty interface and party class

---
+++ CSCD349FinalProject/Characters/IParty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCD349FinalProject.Characters
+{
+    interface IParty
+    {
+        int GetPartyAttack();
+        int GetPartyDefense();
+        int GetLevel();
+        void LevelUp();
+    }
+}
